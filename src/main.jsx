b9d391e01dea7b49ceefa187dbc33cb94e3f1b14
UmeshAhir21 <umesh.it.2125@gmail.com>
2025-05-22 07:55:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
 
-// Ensure you have this in your index.html:
-// <div id="root"></div>
-
 const rootElement = document.getElementById("root");
 
 ReactDOM.createRoot(rootElement).render(
